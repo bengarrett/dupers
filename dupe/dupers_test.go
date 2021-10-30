f2c046cd97c37510dfc686959267dc1774626767
Ben Garrett <code.by.ben@gmail.com>
2021-10-30 12:34:27 +1100

    Parallel tests.

--- dupe/dupers_test.go
+++ dupe/dupers_test.go
@@ -46,6 +46,7 @@ func ExamplePrint() {
 }
 
 func Test_contains(t *testing.T) {
+	t.Parallel()
 	type args struct {
 		s    []string
 		find string
@@ -63,7 +64,9 @@ func Test_contains(t *testing.T) {
 		{"find upper", args{str, "DEF"}, false},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			if got := contains(tt.args.find, tt.args.s...); got != tt.want {
 				t.Errorf("contains() = %v, want %v", got, tt.want)
 			}
@@ -72,6 +75,7 @@ func Test_contains(t *testing.T) {
 }
 
 func Test_containsBin(t *testing.T) {
+	t.Parallel()
 	d, err := filepath.Abs(bucket1)
 	if err != nil {
 		t.Error(err)
@@ -84,7 +88,9 @@ func Test_containsBin(t *testing.T) {
 		{"test dir", d, false},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			if got := containsBin(tt.root); got != tt.want {
 				t.Errorf("containsBin() = %v, want %v", got, tt.want)
 			}
@@ -93,6 +99,7 @@ func Test_containsBin(t *testing.T) {
 }
 
 func Test_read(t *testing.T) {
+	t.Parallel()
 	tests := []struct {
 		name     string
 		path     string
@@ -104,7 +111,9 @@ func Test_read(t *testing.T) {
 		{"file2", file2, hash2, false},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			gotHash, err := read(tt.path)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("read() error = %v, wantErr %v", err, tt.wantErr)
@@ -119,15 +128,18 @@ func Test_read(t *testing.T) {
 }
 
 func Test_SetBuckets(t *testing.T) {
+	t.Parallel()
 	const test = "test"
 	i := internal{}
 	i.SetBuckets(test)
 	t.Run("test set", func(t *testing.T) {
+		t.Parallel()
 		if l := len(i.buckets); l != 1 {
 			t.Errorf("SetBuckets() got = %v, want %v", l, 1)
 		}
 	})
 	t.Run("print", func(t *testing.T) {
+		t.Parallel()
 		if s := i.PrintBuckets(); s != test {
 			t.Errorf("SetBuckets() got = %v, want %v", s, test)
 		}
@@ -135,9 +147,11 @@ func Test_SetBuckets(t *testing.T) {
 }
 
 func Test_SetToCheck(t *testing.T) {
+	t.Parallel()
 	c := Config{}
 	c.SetToCheck(bucket1)
 	t.Run("test set", func(t *testing.T) {
+		t.Parallel()
 		if s := c.source; s == "" {
 			t.Errorf("SetToCheck() got = %v, want the absolute path of: %v", s, bucket1)
 		}
@@ -145,6 +159,7 @@ func Test_SetToCheck(t *testing.T) {
 }
 
 func TestConfig_CheckPaths(t *testing.T) {
+	t.Parallel()
 	type fields struct {
 		Debug    bool
 		Quiet    bool
@@ -169,7 +184,9 @@ func TestConfig_CheckPaths(t *testing.T) {
 		{"okay", f, args{source: bucket2, buckets: []Bucket{bucket1}}, true, 3, 1},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			c := &Config{
 				Debug:    tt.fields.Debug,
 				Quiet:    tt.fields.Quiet,
@@ -193,6 +210,7 @@ func TestConfig_CheckPaths(t *testing.T) {
 }
 
 func TestConfig_Print(t *testing.T) {
+	t.Parallel()
 	b1, _ := filepath.Abs(file1)
 	b2, _ := filepath.Abs(file2)
 	c := Config{}
@@ -207,6 +225,7 @@ func TestConfig_Print(t *testing.T) {
 }
 
 func TestConfig_Remove(t *testing.T) {
+	t.Parallel()
 	c := Config{Test: true}
 	if r := strings.TrimSpace(c.Remove()); r != "No duplicate files to remove." {
 		t.Errorf("Config.Remove() should have returned a nothing to remove message, not %v.", r)
@@ -237,6 +256,7 @@ func TestConfig_Remove(t *testing.T) {
 }
 
 func TestConfig_Clean(t *testing.T) {
+	t.Parallel()
 	c := Config{Test: true}
 	if r := strings.TrimSpace(c.Clean()); r != "" {
 		t.Errorf("Config.Clean() should have returned blank, not %v.", r)
@@ -265,6 +285,7 @@ func TestConfig_Clean(t *testing.T) {
 }
 
 func TestConfig_Status(t *testing.T) {
+	t.Parallel()
 	c := Config{Test: true}
 	c.files = 2
 	const want = "Scanned 2 files"
@@ -274,6 +295,7 @@ func TestConfig_Status(t *testing.T) {
 }
 
 func TestConfig_WalkDirs(t *testing.T) {
+	t.Parallel()
 	var err error
 	c := Config{Test: true, Debug: true}
 	c.db, err = mock.Open()
@@ -286,6 +308,7 @@ func TestConfig_WalkDirs(t *testing.T) {
 }
 
 func TestConfig_WalkDir(t *testing.T) {
+	t.Parallel()
 	var err error
 	c := Config{Test: true, Debug: true}
 	c.db, err = mock.Open()
@@ -309,6 +332,7 @@ func TestConfig_WalkDir(t *testing.T) {
 }
 
 func TestConfig_WalkSource(t *testing.T) {
+	t.Parallel()
 	c := Config{}
 	if err := c.WalkSource(); err == nil {
 		t.Errorf("Config.WalkSource() should return an error with an empty Config.")
@@ -320,6 +344,7 @@ func TestConfig_WalkSource(t *testing.T) {
 }
 
 func Test_printWalk(t *testing.T) {
+	t.Parallel()
 	c := Config{Test: false, Quiet: false, Debug: false}
 	s := strings.TrimSpace(printWalk(false, &c))
 	want := ""
@@ -358,6 +383,7 @@ func Test_printWalk(t *testing.T) {
 }
 
 func TestRemoveAll(t *testing.T) {
+	t.Parallel()
 	c := Config{Test: true, Quiet: false, Debug: true}
 	if err := cleanDir(bucket0); err != nil {
 		t.Error(err)
