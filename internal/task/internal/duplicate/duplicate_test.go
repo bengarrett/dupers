82be43ad57e412003fcbf961f362095b79b11867
Ben Garrett <code.by.ben@gmail.com>
2022-07-08 10:59:38 +1000

    Completed test.

--- internal/task/internal/duplicate/duplicate_test.go
+++ internal/task/internal/duplicate/duplicate_test.go
@@ -2,6 +2,7 @@
 package duplicate_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/bengarrett/dupers/internal/task/internal/duplicate"
@@ -18,12 +19,12 @@ func TestCmdErr(t *testing.T) {
 		args args
 		want string
 	}{
-		{"zero", args{args: 0, buckets: 0, minArgs: 0}, ""},
-		{"one", args{args: 0, buckets: 0, minArgs: 1}, ""},
+		{"zero", args{args: 0, buckets: 0, minArgs: 0}, "database is empty"},
+		{"one", args{args: 0, buckets: 0, minArgs: 1}, "requires a directory or file"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := duplicate.CmdErr(tt.args.args, tt.args.buckets, tt.args.minArgs); got != tt.want {
+			if got := duplicate.CmdErr(tt.args.args, tt.args.buckets, tt.args.minArgs); !strings.Contains(got, tt.want) {
 				t.Errorf("CmdErr() = %v, want %v", got, tt.want)
 			}
 		})
